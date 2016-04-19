674d7232f15e91df09caea16acc4c66b20da1fdd
Lokesh Devnani <lokeshthegenius@gmail.com>
2016-04-19 21:16:00 +0530

    navbar effects added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
+(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+    function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+    e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+    e.src='//www.google-analytics.com/analytics.js';
+    r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+$(document).ready(function(){
 
+    $(".service").click(function(){
+        console.log('clicked');
+        $('.service').removeClass('hovered').removeClass('unhovered').not(this).addClass('closed');
+        $(this).addClass('opened');
+        $(this).find('.service-details').show();
+        $(this).find('.service-header').hide();
+    });
+    $(".service .back-arrow").click(function(e){
+        $(".service").removeClass('opened').removeClass('closed');
+        $(".service-details").hide();
+        $(".service-header").show();
+        e.stopPropagation();
+    });
+});
---
+++ js/plugins.js
@@ -0,0 +1,133 @@
+/**
+ * Created by Lokesh Devnani on 3/3/15.
+ */
+( function( window ) {
+    'use strict';
+    function classReg( className ) {
+        return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
+    }
+
+    var hasClass, addClass, removeClass;
+
+    if ( 'classList' in document.documentElement ) {
+        hasClass = function( elem, c ) {
+            return elem.classList.contains( c );
+        };
+        addClass = function( elem, c ) {
+            elem.classList.add( c );
+        };
+        removeClass = function( elem, c ) {
+            elem.classList.remove( c );
+        };
+    }
+    else {
+        hasClass = function( elem, c ) {
+            return classReg( c ).test( elem.className );
+        };
+        addClass = function( elem, c ) {
+            if ( !hasClass( elem, c ) ) {
+                elem.className = elem.className + ' ' + c;
+            }
+        };
+        removeClass = function( elem, c ) {
+            elem.className = elem.className.replace( classReg( c ), ' ' );
+        };
+    }
+
+    function toggleClass( elem, c ) {
+        var fn = hasClass( elem, c ) ? removeClass : addClass;
+        fn( elem, c );
+    }
+
+    var classie = {
+        // full names
+        hasClass: hasClass,
+        addClass: addClass,
+        removeClass: removeClass,
+        toggleClass: toggleClass,
+        // short names
+        has: hasClass,
+        add: addClass,
+        remove: removeClass,
+        toggle: toggleClass
+    };
+
+// transport
+    if ( typeof define === 'function' && define.amd ) {
+        // AMD
+        define( classie );
+    } else {
+        // browser global
+        window.classie = classie;
+    }
+
+})( window );
+
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        //alert($($anchor.attr("href")).offset().top);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top-10
+        }, 1500, 'easeOutSine');
+        event.preventDefault();
+    });
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+});
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+
+
+/**
+ * cbpAnimatedHeader.js v1.0.0
+ * http://www.codrops.com
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ * Copyright 2013, Codrops
+ * http://www.codrops.com
+ */
+var cbpAnimatedHeader = (function() {
+
+    var docElem = document.documentElement,
+        header = document.querySelector( '.navbar-default' ),
+        didScroll = false,
+        changeHeaderOn = 300;
+
+    function init() {
+        window.addEventListener( 'scroll', function( event ) {
+            if( !didScroll ) {
+                didScroll = true;
+                setTimeout( scrollPage, 250 );
+            }
+        }, false );
+    }
+
+    function scrollPage() {
+        var sy = scrollY();
+        if ( sy >= changeHeaderOn ) {
+            classie.add( header, 'navbar-shrink' );
+        }
+        else {
+            classie.remove( header, 'navbar-shrink' );
+        }
+        didScroll = false;
+    }
+
+    function scrollY() {
+        return window.pageYOffset || docElem.scrollTop;
+    }
+
+    init();
+
+})();
+
